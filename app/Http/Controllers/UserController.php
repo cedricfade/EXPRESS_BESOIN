a8a39824d9fb00b81ebc621c793ed77622f54202
cedricfade <cedricfade55@gmail.com>
2023-06-29 18:48:02 -1200

    AJOUT DE ROUTE1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function Login(){
+
+        return view('page.login');
+
+    }
+
+    public function Inscription(){
+
+
+        return view('page.inscription');
+        
+    }
 }
